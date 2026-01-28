92b85b0cc0544c19b944d06b6d601bc4b0f2aa83
Vincent Brodin <vincent.brodin21@gmail.com>
2026-01-28 14:40:20 +0100

    Init

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Devshell templates";
+
+  outputs = { self }: {
+    templates = {
+      default = {
+        path = ./basic;
+        description = "A basic devShell with standard tools";
+      };
+
+      rust = {
+        path = ./rust;
+        description = "Rust development environment";
+      };
+
+
+      cpp = {
+        path = ./cpp;
+        description = "C++ development environment";
+      };
+    };
+  };
+}
